5f998b81cab1f01d74798daf7fa78691c00c8bf0
Renjith Karimattathil SASIDHARAN <RenjithKarimattathil.SASIDHARAN@amadeus.com>
2022-01-07 19:05:54 +0530

    Model improvements

--- create_ocr_training_data_from_meeter_data.py
+++ create_ocr_training_data_from_meeter_data.py
@@ -7,13 +7,16 @@ import uuid
 input_dir = 'data/tmp/'
 output_dir = 'data/ocr_training_data/'
 
+Path(output_dir).mkdir(parents=True, exist_ok=True) 
+
 with open(input_dir + 'Label.txt') as file:
   labels = file.readlines()
   labels = [labels.rstrip() for labels in labels]
 
-with open(output_dir + 'gt.txt', 'a') as f:
+with open(output_dir + 'gt.txt', 'w') as f:
   for label in labels:
     file_name = label.split('\t')[0]
+    print(file_name)
     gts = json.loads(label.split('\t')[1])
     img = Image.open(input_dir + Path(file_name).name)
 
@@ -27,6 +30,7 @@ with open(output_dir + 'gt.txt', 'a') as f:
 
       gt_file_name = str(uuid.uuid4()) + '.png'
       copped_img = img.crop((x_min, y_min, x_max, y_max))
-      copped_img.save(output_dir + gt_file_name, 'PNG')
+      resized_img = copped_img.resize((200, 31), Image.LANCZOS)
+      resized_img.save(output_dir + gt_file_name, 'PNG')
 
       f.write(gt_file_name + '\t' + text + '\n')
